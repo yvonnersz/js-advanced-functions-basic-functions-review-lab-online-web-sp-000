1a7d12bd2c3d0583b96e2a3c346ced412b80315f
Yvonne M Pham <yvonnersz@gmail.com>
2020-08-06 03:09:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ function saturdayFun(activity="roller-skate") {
 function mondayWork(activity="go to the office") {
   return (`This Monday, I will ${activity}.`)
 }
+
+function wrapAdjective(string="*") {
+  return function(adjective="special") {
+    return `You are ${string}${adjective}${string}.`
+  }
+}
+
+wrapAdjective()("a dedicated programmer");
