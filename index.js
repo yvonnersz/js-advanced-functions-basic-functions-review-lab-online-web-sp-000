f13a46f8ed2d4e3636a49c49a50b4b7e0e346c8a
Yvonne M Pham <yvonnersz@gmail.com>
2020-08-06 02:20:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@
 function saturdayFun(activity="roller-skate") {
   return (`This Saturday, I want to ${activity}!`);
 }
+
+function mondayWork(activity="go to the office") {
+  return (`This Monday, I will ${activity}.`)
+}
